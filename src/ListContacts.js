2ce3dcc2db129be4c758f4fd3214177b005d27df
pleaobraga <pleao.braga@gmail.com>
2017-11-06 22:28:27 -0200

    filter contacts

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import escapeRegExp from 'escape-string-regexp'
+import sortBy from 'sort-by'
 
 class ListContacts extends Component {
 
@@ -8,10 +10,38 @@ class ListContacts extends Component {
         onDeleteContact: PropTypes.func.isRequired
     }
 
+    state = {
+        query: ''
+    }
+
+    updateQuery = (query) => {
+        this.setState({ query: query.trim() })
+    }
+
     render() {
+
+        let showingContacts;
+        if (this.state.query) {
+            const match = new RegExp(escapeRegExp(this.state.query), 'i');
+            showingContacts = this.props.contacts.filter((contact) => match.test(contact.name));
+        } else {
+            showingContacts = this.props.contacts;
+        }
+
+        showingContacts.sort(sortBy('name'))
+
         return (
-            <ol className='contact-list'>
-                {this.props.contacts.map((contact) => (
+            <div className='list-contacts' >
+                <div className='list-contacts-top' >
+                    <input className="search-contacts"
+                        type="text"
+                        placeholder="Search contacts"
+                        value={this.state.query}
+                        onChange={(event) => this.updateQuery(event.target.value)}
+                     />
+                </div>
+                <ol className='contact-list'>
+                {showingContacts.map((contact) => (
                     <li key={contact.id} className='contact-list-item' >
                         <div className='contact-avatar' style={{
                             backgroundImage: `url(${contact.avatarURL})`
@@ -26,6 +56,8 @@ class ListContacts extends Component {
                     </li>
                 ))}
             </ol>
+            </div>
+            
         )
     }
 
